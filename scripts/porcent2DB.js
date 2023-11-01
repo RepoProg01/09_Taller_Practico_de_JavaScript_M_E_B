e1eed3fee25395c9b33c8143e300863857badcf4
Alexhpdv4 <alexlougog34@gmail.com>
2023-11-01 06:45:21 -0500

    menu v61

--- scripts/porcent2DB.js
+++ scripts/porcent2DB.js
@@ -7,16 +7,16 @@ const introPorcentaje = {
     defPorcentajeTitulo: "Que es porcentaje ?",
     defPorcentaje: "El porcentaje es la forma de expresar un número como una fracción que tiene como denominador el número 100, conocido también, como tanto por ciento. ",
     defPorcentaje2Titulo: "Regla de tres",
-    defPorcentaje2: "La regla de tres puede ser:<br><br>Simple directa,<br>Simple inversa,<br>Compuesta directa,<br>Compuesta inversa.",
+    defPorcentaje2: "La regla de tres es una forma de resolver problemas de proporcionalidad entre tres valores conocidos y una incógnita. En ella se establece una relación de linealidad, proporcionalidad, entre los valores involucrados.",
 
-    defNotaTitulo: "Regla de tres Compuesta",
-    defNota: "Simple directa es cuando son directamente proporcionales.<br><br>Simple inversa es cuando sus magnitudes inversamente proporcionales,<br>",
+    defNotaTitulo: "Directa o Inversa",
+    defNota: "La relación de proporcionalidad puede ser directa o inversa. Será directa cuando a un mayor valor de A habrá un mayor valor de B, y será inversa cuando a un mayor valor de A corresponda un menor valor de B.",
     defGuiaTitulo: "Forma de uso :",
-    defGuia: "Con el boton de <span>Casos</span> seleccionas diferentes variantes de la regla de tres y mostrara calculadoras.<br>Con el boton <span>Menu</span> regrezaras al menu principal."
+    defGuia: "Con el boton de <span>Mas..</span> mostrara calculadoras y ejemplos de diferentes casos de la regla de tres,<br>Con el boton <span>Menu</span> regrezaras al menu principal."
 };
 
 const RD3SimpleDirectaObj = {
-    id: "simple_directa",
+    id: "ejemplo_simple_directa",
     titleFig: "Simple directa",
     imgFig: "../assets/SimpleDirecta.png",
 
@@ -45,8 +45,37 @@ const RD3SimpleDirectaObj = {
     funcionPorcentaje: "pcSD",
 };
 const RD3SimpleInversaObj = {
-    id: "simple_inversa",
+    id: "ejemplo_simple_inversa",
     titleFig: "Simple inversa",
+    imgFig: "../assets/SimpleInversa.png",
+
+    titlePorcentaje: "Regla de tres Simple inversa",
+    formulaPorcentaje: "Inserta los 3 datos conocidos",
+    inputPorcentaje: [
+        {
+            inputLabel: "A",
+            inputId: "winSIvalorA"
+        },
+        {
+            inputLabel: "B",
+            inputId: "winSIvalorB"
+        },
+        {
+            inputLabel: "C",
+            inputId: "winSIvalorC"
+        },
+        {
+            inputLabel: "D",
+            inputId: "winSIvalorD"
+        }
+    ],
+    resultClPC: "resultPCSI",
+    btnClPC: "btnResultPCSI",
+    funcionPorcentaje: "pcSI",
+};
+const RD3CompuestaDirectaObj = {
+    id: "ejemplo_compuesta_directa",
+    titleFig: "Compuesta directa",
     imgFig: "../assets/SimpleInversa.png",
 
     titlePorcentaje: "Regla de tres Simple inversa",
@@ -77,6 +106,7 @@ const RD3SimpleInversaObj = {
 introPorcentajeArray.push(introPorcentaje);
 introPorcentajeArray.push(RD3SimpleDirectaObj);
 introPorcentajeArray.push(RD3SimpleInversaObj);
+introPorcentajeArray.push(RD3CompuestaDirectaObj);
 
 
 
